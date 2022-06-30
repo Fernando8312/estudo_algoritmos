fea258c5f015d4d7cec816f3de7e3c35254b743e
Fernando Eduardo <lima8312@hotmail.com>
2022-06-30 14:49:04 -0300

    Primeiro Commit

---
+++ bubble_sort.py
@@ -0,0 +1,17 @@
+import random
+l = random.sample(range(1199),20)
+print(l)
+
+def ordena(lista):
+    for i in range(len(lista)-1):
+        for j in range(len(lista) - i - 1):
+            if lista[j] > lista[j + 1]:
+                temp = lista[j]
+                lista[j] = lista[j + 1]
+                lista[j + 1] = temp
+                #print(lista)
+    return lista
+print(ordena(l))
+
+#print(ordena([100,99,98]))
+
